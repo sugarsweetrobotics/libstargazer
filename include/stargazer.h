5ade3e05086f25b36df4879a21658ccc1ed8f453
Yuki Suga <ysuga@ysuga.net>
2015-09-16 10:07:02 +0900

    Update for libaqua

---
+++ include/stargazer.h
@@ -0,0 +1,15 @@
+/**
+ * stargazer.h
+ * libstargazer header file
+ * date: 2015/09/16
+ * author: Yuki Suga (SUGAR SWEET ROBOTICS)
+ * email: ysuga@sugarsweetrobotics.com
+ * url: https://github.com/sugarsweetrobotics/libstargazer.git
+ * copyright: 2015, Yuki Suga, Sugar Sweet Robotics all rights reserved.
+ * license: GPL
+ */
+
+#pragma once 
+
+
+
